89fc68ccc2b85d027ee79577b4e54f0caf1085d7
shariqanwar20 <shariq.anwar@nixorcollege.edu.pk>
2024-03-02 22:54:08 +0500

    fetch creds from .env .env.example file attached

--- services/workspace-service/src/utils/db.ts
+++ services/workspace-service/src/utils/db.ts
@@ -1,7 +1,7 @@
 import * as AWS from 'aws-sdk';
 
-// set profile - local testing
-AWS.config.credentials = new AWS.SharedIniFileCredentials({ profile: 'ncbp' });
+// get credentials from environment variables
+// AWS.config.credentials = new AWS.SharedIniFileCredentials({ profile: 'ncbp' });
 
 const dynamoDB = new AWS.DynamoDB.DocumentClient({
     region: "us-east-1"
