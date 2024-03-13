6a23f68ba468b78373756574da167c95fe351e96
Zohaib Azam <66849463+Zohaib58@users.noreply.github.com>
2024-03-13 21:34:46 +0500

    Add aws-jwt-verify package for token verification

--- services/dataset-services/src/auth/auth.middleware.ts
+++ services/dataset-services/src/auth/auth.middleware.ts
@@ -2,56 +2,68 @@ import { HttpException, HttpStatus, Injectable, NestMiddleware } from '@nestjs/c
 import * as jwt from 'jsonwebtoken';
 import { DynamoDB } from 'aws-sdk';
 import { NextFunction } from 'express';
-import { ConfigService } from '@nestjs/config';
+import { CognitoJwtVerifier } from "aws-jwt-verify";
+
 
 @Injectable()
 export class AuthMiddleware implements NestMiddleware {
   private readonly dynamoDb: DynamoDB.DocumentClient;
-  private configService: ConfigService;
-
+  
   constructor() {
     this.dynamoDb = new DynamoDB.DocumentClient();
   }
 
   async use(req: Request, res: Response, next: NextFunction) {
-    //let headers = new Headers();
-    // const headers = req.headers as { authorization?: string }
-    // if (headers.authorization && headers.authorization.startsWith('Bearer'))
-    // {
-    //   const token = headers.authorization.split(' ')[1];
-    //   if (!token) {
-    //     throw new HttpException('No token provided', HttpStatus.UNAUTHORIZED);
-    //   }
-    //   const secret = process.env.JWT_SECRET;
-    //   // Verify token
-    //   const decoded = jwt.verify(token, secret)
+    const headers = req.headers as { authorization?: string }
+    if (headers.authorization && headers.authorization.startsWith('Bearer'))
+    {
+      const token = headers.authorization.split(' ')[1];
+      if (!token) {
+        throw new HttpException('No token provided', HttpStatus.UNAUTHORIZED);
+      }
+      // Verify token
+      const decoded = jwt.decode(token)
+      // Verifier that expects valid access tokens:
+    const verifier = CognitoJwtVerifier.create({
+      userPoolId: process.env.USER_POOL_ID,
+      tokenUse: "id",
+      clientId: process.env.CLIENT_ID,
+    });
+
+    try {
+      const payload = await verifier.verify(
+        token
+      );
+      console.log(payload)
       next(); // Call next route handler
-    // }
+    } catch {
+      console.log("Token not valid!");
+    }
     
-    // try {
-    //   const decoded: any = jwt.verify(token, process.env.JWT_SECRET);
-    //   if (!decoded.sub) {
-    //     throw new HttpException('Invalid token: sub not found', HttpStatus.UNAUTHORIZED);
-    //   }
-
-    //   // Define DynamoDB parameters
-    //   const params: DynamoDB.DocumentClient.GetItemInput = {
-    //     TableName: this.configService.get<string>('DYNAMODB_TABLE_NAME'),
-    //     Key: {
-    //       'sub': decoded.sub,
-    //     },
-    //   };
-
-    //   // Fetch user details from DynamoDB table
-    //   const data = await this.dynamoDb.get(params).promise();
-
-    //   if (!data) {
-    //     throw new HttpException('User not found', HttpStatus.NOT_FOUND);
-    //   }
-
-    // } catch (error) {
-    //   console.error(error);
-    //   throw new HttpException('Invalid token', HttpStatus.UNAUTHORIZED);
-    // }
+     
+    }
   }
 }
+/*
+if (decoded.exp && Date.now() >= decoded.exp * 1000) {
+        throw new Error('Token expired');
+      }
+      //Define DynamoDB parameters
+      const params: DynamoDB.DocumentClient.GetItemInput = {
+        TableName: process.env.DYNAMODB_TABLE_NAME,
+        Key: {
+          'PK': `USER#${payload.sub}`,
+          'SK': `USEREMAIL#${payload.email}`,
+        },
+      };
+
+      // Fetch User
+      try {
+        const data = await this.dynamoDb.get(params).promise();
+        
+      }
+      catch{
+        console.log("User not found!");
+      }
+      
+*/
