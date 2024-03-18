ed02ca85aec1cfe966236fa37b2fe7ec399488df
shariqanwar20 <shariq.anwar@nixorcollege.edu.pk>
2024-03-18 16:44:36 +0500

    updated ports in docker file and added a health path

--- services/workspace-service/src/server.ts
+++ services/workspace-service/src/server.ts
@@ -4,12 +4,16 @@ import 'dotenv/config';
 import { authenticate } from './middlewares/authentication.middleware';
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port = 80
 
 app.use(express.json());
 
+app.get('/workspaces/health', (req, res) => {
+  res.send('<h1>Workspace Service is running</h1>');
+});
 
 app.use('/workspaces', authenticate, workspaceRouter);
+
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
--- services/workspace-service/src/utils/db.ts
+++ services/workspace-service/src/utils/db.ts
@@ -4,7 +4,7 @@ import * as AWS from 'aws-sdk';
 // AWS.config.credentials = new AWS.SharedIniFileCredentials({ profile: 'ncbp' });
 
 const dynamoDB = new AWS.DynamoDB.DocumentClient({
-    region: "us-east-1"
+    region: "us-east-1",
 });
 
 export { dynamoDB };
