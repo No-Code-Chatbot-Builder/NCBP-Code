2b71ea03289da536fca85fba0bda3b03971a78ce
Zohaib Azam <66849463+Zohaib58@users.noreply.github.com>
2024-04-09 21:35:01 +0500

    Enable CORS in main.ts

--- services/bot_service/src/main.ts
+++ services/bot_service/src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({origin: 'http://localhost:3009'});
+  app.enableCors();
   await app.listen(3005);
 }
 bootstrap();
