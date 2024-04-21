189e1940a834d752fbff47cbb5a31dd271325bad
Hussain Murtaza <hash.hussain53@gmail.com>
2024-04-21 11:30:40 +0500

    pull :  removed inconsisitency by ibrahim

--- services/frontend/src/providers/redux/store/store.ts
+++ services/frontend/src/providers/redux/store/store.ts
@@ -15,36 +15,5 @@ export const store = configureStore({
   },
 });
 
-<<<<<<< HEAD
-const persistConfig = {
-  key: "root",
-  storage,
-  // whitelist: ["workspaces", "assistants", "datasets", "chatbot", "featured"],
-};
-
-const makeConfiguredStore = () =>
-  configureStore({
-    reducer: rootReducer,
-  });
-
-export const makeStore = () => {
-  const isServer = typeof window === "undefined";
-  if (isServer) {
-    return makeConfiguredStore();
-  } else {
-    const persistedReducer = persistReducer(persistConfig, rootReducer);
-    let store: any = configureStore({
-      reducer: persistedReducer,
-    });
-    store.__persistor = persistStore(store);
-    return store;
-  }
-};
-
-export type AppStore = ReturnType<typeof makeStore>;
-export type RootState = ReturnType<AppStore["getState"]>;
-export type AppDispatch = AppStore["dispatch"];
-=======
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
->>>>>>> 8173deb279e81c88256174682beca7ce7ac4ae41
