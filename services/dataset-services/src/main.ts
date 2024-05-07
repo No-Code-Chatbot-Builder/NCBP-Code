63d665791cdc713a3c5048397e9ce97bb5bd856b
Zohaib Azam <66849463+Zohaib58@users.noreply.github.com>
2024-05-07 14:13:18 +0500

    Update main.ts to set environment variables before initializing the app and adjust CORS policy

--- services/dataset-services/src/main.ts
+++ services/dataset-services/src/main.ts
@@ -1,9 +1,35 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+export const setEnvironmentVariables = () => {
+  try {
+    const secret = process.env.SECRET;
+    if (!secret) {
+      throw new Error('SECRET environment variable is not provided.');
+    }
+
+    const secretJson = JSON.parse(secret);
+    const requiredKeys = ['PINECONE_API_KEY', 'DYNAMODB_TABLE_NAME', 'S3_BUCKET_NAME', 'PINECONE_INDEX_NAME', 'USER_POOL_ID', 'CLIENT_ID', 'AWS_ACCESS_KEY_ID', 'AWS_SECRET_ACCESS_KEY', 'AWS_DEFAULT_REGION'];
+
+    requiredKeys.forEach(key => {
+      if (!secretJson[key]) {
+        throw new Error(`Required key not found in secret: ${key}`);
+      }
+      process.env[key] = secretJson[key];
+    });
+  
+  } catch (error) {
+    console.error(error);
+    process.exit(1); // Exit the application if secrets can't be loaded.
+  }
+};
+
 async function bootstrap() {
+  setEnvironmentVariables(); // Set environment variables before initializing the app
+
   const app = await NestFactory.create(AppModule);
-  app.enableCors( {origin: 'http://localhost:3000'});
-  await app.listen(80);
+  app.enableCors({ origin: 'http://localhost:3000' }); // Adjust CORS policy
+  await app.listen(80); // This will use the PORT environment variable if set
 }
+
 bootstrap();
