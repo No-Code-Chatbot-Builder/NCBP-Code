1c81003bdb579c3ab407f211003c701d8d1d079c
shariqanwar20 <shariq.anwar@nixorcollege.edu.pk>
2024-05-11 23:02:42 +0500

    removed setEnv added cors

--- services/key-management-service/src/server.ts
+++ services/key-management-service/src/server.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import keyRouter from './routers/domain';
+import cors from 'cors';
 import 'dotenv/config';
 import { authenticate } from './middlewares/authentication.middleware';
 import { setEnviromentVariables } from './utils/helpers';
@@ -7,10 +8,10 @@ import { setEnviromentVariables } from './utils/helpers';
 const app = express();
 const port = process.env.PORT || 5000;
 
-if (process.env.NODE_ENV === 'prod') {
-  setEnviromentVariables();
-}
-
+// if (process.env.NODE_ENV === 'prod') {
+//   setEnviromentVariables();
+// }
+app.use(cors())
 app.use(express.json());
 
 app.use('/domains', authenticate, keyRouter);
