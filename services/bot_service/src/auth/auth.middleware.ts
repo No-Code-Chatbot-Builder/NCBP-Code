9b29ad42ab6a6663451703ce70d5b238f1f43ea8
iamlabibasif <u.asif.21749@khi.iba.edu.pk>
2024-05-13 15:06:55 +0500

    Middleware update with iFrame

--- services/bot_service/src/auth/auth.middleware.ts
+++ services/bot_service/src/auth/auth.middleware.ts
@@ -4,24 +4,59 @@ import { CognitoJwtVerifier } from 'aws-jwt-verify';
 import { SimpleJwksCache } from 'aws-jwt-verify/jwk';
 import { SimpleJsonFetcher } from 'aws-jwt-verify/https';
 import * as AWS from 'aws-sdk';
+import { HttpService } from '@nestjs/axios';
+import { AxiosResponse } from 'axios';
+
+
+// Extend Request to include custom query parameters
+interface CustomRequest extends Request {
+  query: {
+    botId: string;
+    workspaceId: string;
+  };
+}
 
 @Injectable()
 export class AuthMiddleware implements NestMiddleware {
   private dynamodb: AWS.DynamoDB.DocumentClient;
 
-  constructor() {
+  constructor(private httpService: HttpService) {
     this.dynamodb = new AWS.DynamoDB.DocumentClient({
       region: 'us-east-1'
     });
   }
 
-  async use(req: Request, res: Response, next: NextFunction) {
-    const headers = req.headers as { authorization?: string };
+  async use(req: CustomRequest, res: Response, next: NextFunction) {
+    // Extract botId and workspaceId from request
+    const { botId, workspaceId } = req.query;
+
+    // Fetch domains
+    try {
+        const domainResponse: AxiosResponse = await this.httpService.get('http://localhost:80/domains/', {
+            params: { botId, workspaceId }
+        }).toPromise();
+
+        const domains = domainResponse.data.resultDomain.domains; // Assuming domains are in this path
+        const requestOrigin = req.headers['origin'] as string | undefined; // Define requestOrigin here
+
+        // Check if origin is in allowed domains
+        if (domains.includes(requestOrigin)) {
+            next(); // If origin matches, proceed without token verification
+            return;
+        }
+    } catch (error) {
+        console.error('Error fetching domains:', error);
+        throw new HttpException('Failed to fetch domains', HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    // Continue with the token verification process only if origin doesn't match
+    const headers = req.headers as { authorization?: string }; // Define headers here
     if (headers.authorization && headers.authorization.startsWith('Bearer')) {
-      const token = headers.authorization.split(' ')[1];
-      if (!token) {
-        throw new HttpException('No token provided', HttpStatus.UNAUTHORIZED);
-      }
+        const token = headers.authorization.split(' ')[1]; // Extract token here
+        if (!token) {
+            throw new HttpException('No token provided', HttpStatus.UNAUTHORIZED);
+        }
+
       // Verifier that expects valid access tokens:
       const verifier = CognitoJwtVerifier.create(
         {
@@ -71,4 +106,4 @@ export class AuthMiddleware implements NestMiddleware {
       }
     }
   }
-}
+  }
