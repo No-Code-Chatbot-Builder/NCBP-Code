77cee490c48608bad41ab914aa2b284a5fa74866
iamlabibasif <u.asif.21749@khi.iba.edu.pk>
2024-05-23 00:38:23 +0500

    port updated

--- services/bot_service/src/main.ts
+++ services/bot_service/src/main.ts
@@ -38,6 +38,6 @@ async function bootstrap() {
   setEnvironmentVariables();
   const app = await NestFactory.create(AppModule);
   app.enableCors({ origin: 'http://localhost:3000' });
-  await app.listen(3007);
+  await app.listen(80);
 }
 bootstrap();
