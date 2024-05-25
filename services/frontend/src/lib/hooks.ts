e91cd4dc0f22339b8c0fd1e3778148948d5e77cd
Ibrahim Sheikh <ibrahimtariqsheikh@gmail.com>
2024-05-26 01:29:23 +0500

    build test 3

--- services/frontend/src/lib/hooks.ts
+++ services/frontend/src/lib/hooks.ts
@@ -1,11 +1,6 @@
 import { useDispatch, useSelector, useStore } from "react-redux";
 import type { TypedUseSelectorHook } from "react-redux";
-import type {
-  RootState,
-  AppDispatch,
-  AppStore,
-} from "@/providers/redux/store/store";
+import type { RootState, AppDispatch } from "@/providers/redux/store/store";
 
 export const useAppDispatch: () => AppDispatch = useDispatch;
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
-export const useAppStore: () => AppStore = useStore;
