5239a966ebceba87f74405b23af75a25ef57d72c
iamlabibasif <u.asif.21749@khi.iba.edu.pk>
2024-05-27 17:11:37 +0500

    updated datasetId mistake

--- services/bot_service/src/bot/bot.service.ts
+++ services/bot_service/src/bot/bot.service.ts
@@ -117,7 +117,7 @@ export class BotService implements OnGatewayConnection, OnGatewayDisconnect {
       includeMetadata: true,
       filter: {
         workspaceId: workspaceId,
-        dataSetId: dataSetId,
+        datasetId: dataSetId,
       },
     });
 
